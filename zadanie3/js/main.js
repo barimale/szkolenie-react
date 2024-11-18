565418254e023aab6afa96969bdb2d51ce093240
Mateusz <mateusz.wolnica@gmail.com>
2024-11-18 13:45:09 +0100

    .

---
+++ zadanie3/js/main.js
@@ -0,0 +1,11 @@
+const products = [
+  { name: "Laptop", price: 3000 },
+  { name: "Phone", price: 1500 },
+  { name: "Tablet", price: 1000 }
+];
+
+const names = products.map((product)=>{
+  return product.name.toUpperCase();
+});
+
+console.log(names);
