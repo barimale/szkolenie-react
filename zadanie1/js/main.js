960b89521a2b5c2f3d1ed52c642605cee07a0c95
Mateusz <mateusz.wolnica@gmail.com>
2024-11-18 12:40:11 +0100

    .

---
+++ zadanie1/js/main.js
@@ -0,0 +1,7 @@
+const people = [
+    { name: "Jan", age: 25, city: "Warszawa" },
+    { name: "Anna", age: 30, city: "Kraków" },
+    { name: "Marek", age: 22, city: "Gdańsk" },
+  ];
+
+  console.log(people)
