47222574a8f29685ca01616790ab93df76420e9d
Mateusz <mateusz.wolnica@gmail.com>
2024-11-18 13:55:11 +0100

    .

---
+++ zadanie6/js/main.js
@@ -0,0 +1,21 @@
+const people = [
+  { name: "John", age: 25, hobbies: ["reading", "sports"] },
+  { name: "Anna", age: 32, hobbies: ["cooking", "dancing"] },
+  { name: "Mike", age: 20, hobbies: ["gaming", "cycling"] }
+];
+
+const [john, anna, mike] = people;
+
+const { nameJohn, ageJohn } = john;
+const { nameAnna, ageAnna } = anna;
+const { nameMike, ageMike } = john;
+
+const filtered = people
+  .filter((person)=>{
+    return person.age > 21;
+  })
+  .map((person)=>{
+    return person.name;
+  });
+
+  // zdarzenie do uzupelnienia
