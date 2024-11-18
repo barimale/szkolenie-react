7b4371911fc48330acf99e5249f76a74c7d6ff7a
Mateusz <mateusz.wolnica@gmail.com>
2024-11-18 13:37:02 +0100

    .

---
+++ zadanie2/js/main.js
@@ -0,0 +1,10 @@
+const book = ["1984", "George Orwell", "Dystopia", 328];
+
+const [title, author, , pages] = book;
+
+function Print(title, author, pages)
+{
+  console.log(`Książka "${title}" napisana przez ${author} ma ${pages} stron.`);
+}
+
+Print(title, author, pages);
