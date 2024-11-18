fd166c95b491904a753195451e0379cd2204bb8a
Mateusz <mateusz.wolnica@gmail.com>
2024-11-18 19:36:57 +0100

    .

--- zadanie5/js/main.js
+++ zadanie5/js/main.js
@@ -2,12 +2,16 @@
     return Math.min(Math.floor(Math.random() * 100) + 1, 100);
   }
 
-  var counter = 3;
-  document.querySelector("button").addEventListener("click", (e)=>{
+  function myClick(event) {
     document.querySelector("p").nodeValue = getRandomInt();
     counter = counter -1;
     if(counter === 0)
     {
-      document.querySelector("button").removeEventListener("click");
+      document.querySelector("button").removeEventListener("click", myClick);
     }
+    }
+
+  var counter = 3;
+  document.querySelector("button").addEventListener("click", (e)=>{
+    myClick(e);
   });
