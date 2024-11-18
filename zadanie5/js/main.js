e27cc42438e561cbbb8e235fa6da4a4bc4fd5467
Mateusz <mateusz.wolnica@gmail.com>
2024-11-18 19:28:58 +0100

    .

--- zadanie5/js/main.js
+++ zadanie5/js/main.js
@@ -1,8 +1,13 @@
-  // zdarzenie do uzupelnienia
-  function getRandomInt() {
+    function getRandomInt() {
     return Math.min(Math.floor(Math.random() * 100) + 1, 100);
   }
 
-  const randomNumber = getRandomInt();
-
-  console.log(randomNumber);
+  var counter = 3;
+  document.querySelector("button").addEventListener("click", (e)=>{
+    document.querySelector("p").nodeValue = getRandomInt();
+    counter = counter -1;
+    if(counter === 0)
+    {
+      document.querySelector("button").removeEventListener("click");
+    }
+  });
