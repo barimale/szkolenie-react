e882d21cb77ceae57a9097f7f79056cc6cc15a48
Mateusz <mateusz.wolnica@gmail.com>
2024-11-19 13:10:20 +0100

    .

---
+++ zadanie2_1/js/main.js
@@ -0,0 +1,31 @@
+const people = [
+  { name: "John", age: 25, hobbies: ["reading", "sports"] },
+  { name: "Anna", age: 32, hobbies: ["cooking", "dancing"] },
+  { name: "Mike", age: 20, hobbies: ["gaming", "cycling"] }
+];
+
+const [john, anna, mike] = people;
+
+const { nameJohn, ageJohn } = john;
+const { nameAnna, ageAnna } = anna;
+const { nameMike, ageMike } = mike;
+
+const filtered = people
+  .filter((person) => {
+    return person.age > 21;
+  })
+  .map((person) => {
+    return person.name;
+  });
+
+function myClick(event) {
+  const ulElement = document.querySelector('ul');
+  filtered.forEach((item) => {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(item));
+    ulElement.appendChild(li);
+  })
+  document.querySelector("button").removeEventListener("click", myClick);
+}
+
+document.querySelector("button").addEventListener("click", myClick);
