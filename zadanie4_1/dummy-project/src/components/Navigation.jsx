e1cb1d70f52538d5a01eff0ac2be331a4078ddb1
Mateusz <mateusz.wolnica@gmail.com>
2024-11-21 21:30:43 +0100

    .

--- zadanie4_1/dummy-project/src/components/Navigation.jsx
+++ zadanie4_1/dummy-project/src/components/Navigation.jsx
@@ -3,11 +3,13 @@ import Link from './Link';
 const Navigation = () =>{
     return (
         <nav>
-            {[...Array(4)].map((_, index)=>{
-                return <li key={index}>
-                    <Link />
-                </li>
-            })}
+            <ul>
+                {[...Array(4)].map((_, index)=>{
+                    return <li key={index}>
+                        <Link />
+                    </li>
+                })}
+            </ul>
         </nav>
     );
 }
