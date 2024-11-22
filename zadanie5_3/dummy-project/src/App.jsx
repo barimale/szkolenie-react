dbea4c9cdffe264668a3e6eac2e4a02b44ae6065
Mateusz <mateusz.wolnica@gmail.com>
2024-11-22 16:48:55 +0100

    .

--- zadanie5_3/dummy-project/src/App.jsx
+++ zadanie5_3/dummy-project/src/App.jsx
@@ -7,7 +7,10 @@ import ProductList from './components/ProductList';
 function App() {
   const items = [
     { id: 1, name: "Laptop", price: 3000 },
-    { id: 2, name: "Smartfon", price: 2000 }
+    { id: 2, name: "Smartfon", price: 2000 },
+    { id: 3, name: "Smartfon2", price: 12000 },
+    { id: 4, name: "Smartfon3", price: 1000 }
+
   ];
 
   return (
