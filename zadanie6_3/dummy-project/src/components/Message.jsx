e51d4f41fd16f9df1df2887069cf088f706e6b3c
Mateusz <mateusz.wolnica@gmail.com>
2024-11-25 13:18:50 +0100

    .

--- zadanie6_3/dummy-project/src/components/Message.jsx
+++ zadanie6_3/dummy-project/src/components/Message.jsx
@@ -4,42 +4,42 @@ const Message = () => {
     const [textStyle, setTextStyle] = useState({
         fontSize: 16, // w pikselach
         fontWeight: 'normal', // normalne lub pogrubione
-         fontStyle: 'normal', // normalne lub kursywa
+        fontStyle: 'normal', // normalne lub kursywa
     });
 
     const sizeUp = () => {
-        setTextStyle({...textStyle,fontSize: textStyle.fontSize + 2});
+        setTextStyle({ ...textStyle, fontSize: textStyle.fontSize + 2 });
 
     }
 
-    const sizeDown =() =>{
-        setTextStyle({...textStyle,fontSize: textStyle.fontSize - 2});
+    const sizeDown = () => {
+        setTextStyle({ ...textStyle, fontSize: textStyle.fontSize - 2 });
     }
 
-    const weightNormal = () =>{
-        setTextStyle({...textStyle,fontWeight: 'normal'});
+    const weightNormal = () => {
+        setTextStyle({ ...textStyle, fontWeight: 'normal' });
     }
 
-    const weightBold = () =>{
-        setTextStyle({...textStyle,fontWeight: 'bold'});
+    const weightBold = () => {
+        setTextStyle({ ...textStyle, fontWeight: 'bold' });
     }
 
-    const styleItalic = () =>{
-        setTextStyle({...textStyle,fontStyle: 'italic'});
+    const styleItalic = () => {
+        setTextStyle({ ...textStyle, fontStyle: 'italic' });
     }
 
-    const styleNormal = () =>{
-        setTextStyle({...textStyle,fontStyle: 'normal'});
+    const styleNormal = () => {
+        setTextStyle({ ...textStyle, fontStyle: 'normal' });
     }
-    
+
     return (
         <>
-        <button onClick={sizeUp}>Powiększ tekst</button>
-        <button onClick={sizeDown}>Pomniejsz tekst</button>
-        <button onCLick={weightBold}>Pogrub</button>
-        <button onClick={weightNormal}>Odchudź</button>
-        <button onClick={styleItalic}>Kursywa</button>
-        <button onClick={styleNormal}>Normalny</button>
+            <button onClick={sizeUp}>Powiększ tekst</button>
+            <button onClick={sizeDown}>Pomniejsz tekst</button>
+            <button onClick={weightBold}>Pogrub</button>
+            <button onClick={weightNormal}>Odchudź</button>
+            <button onClick={styleItalic}>Kursywa</button>
+            <button onClick={styleNormal}>Normalny</button>
 
             <p style={{
                 fontSize: textStyle.fontSize,
