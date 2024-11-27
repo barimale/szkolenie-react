fd9954152256af5b0c488e40509c5720ce5f1621
Mateusz <mateusz.wolnica@gmail.com>
2024-11-27 17:40:16 +0100

    .

---
+++ zadanie8_2/dummy-project/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Sizer from './components/Sizer';
+
+function App() {
+
+  return (
+    <>
+      <header>
+      </header>
+      <Sizer />
+      <footer>
+      </footer>
+    </>
+  )
+}
+
+export default App
