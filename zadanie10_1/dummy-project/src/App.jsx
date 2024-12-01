e27cf0cbc660ff6321970ac5257b19c98d3395b3
Mateusz <mateusz.wolnica@gmail.com>
2024-12-01 14:13:49 +0100

    .

--- zadanie10_1/dummy-project/src/App.jsx
+++ zadanie10_1/dummy-project/src/App.jsx
@@ -15,7 +15,6 @@ function App() {
   useEffect(() => {
     const storedValue = localStorage.getItem('movies');
     if (storedValue) {
-      console.log(JSON.parse(storedValue));// wykonuje sie dwa razy, za drugim razem pusty storedValue
       setMovies(JSON.parse(storedValue));
     }
   }, []);
