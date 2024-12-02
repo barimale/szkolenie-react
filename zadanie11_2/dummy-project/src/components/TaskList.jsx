8b9f48b56c0b39aa087a9325c9ac468d203001de
Mateusz <mateusz.wolnica@gmail.com>
2024-12-02 13:06:04 +0100

    .

--- zadanie11_2/dummy-project/src/components/TaskList.jsx
+++ zadanie11_2/dummy-project/src/components/TaskList.jsx
@@ -4,11 +4,11 @@ import { useState, useCallback } from 'react';
 const TaskList = (props) => {
     const [tasks, useTasks] = useState(props.tasks);
 
-    const deleteItem = (itemId) => {
+    const deleteItem = useCallback((itemId) => {
         const filtered = tasks.filter(p => p.id !== itemId);
         console.log(JSON.stringify(filtered));
         useTasks(filtered);
-    };
+      }, [tasks])
 
     return (
         <>
