8c1174fe079bb8bb2b2d5777bf5b69779e27a277
Mateusz <mateusz.wolnica@gmail.com>
2024-12-02 12:43:54 +0100

    .

--- zadanie11_1/dummy-project/src/App.jsx
+++ zadanie11_1/dummy-project/src/App.jsx
@@ -1,12 +1,26 @@
+import { useRef, useEffect } from 'react';
 import './App.css'
 
 function App() {
+  const inputRef = useRef(undefined);
+  useEffect(()=>{
+    if(inputRef.current)
+    {
+      inputRef.current.focus();
+    }
+  }, []);
 
   return (
     <>
       <header>
       </header>
-      
+      <label htmlFor="title">Tytuł</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          ref={inputRef}
+        />
       <footer>
       </footer>
     </>
