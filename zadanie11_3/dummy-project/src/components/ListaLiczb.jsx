081cf8cba17a0d32e28508e7d761fd439edf290e
Mateusz <mateusz.wolnica@gmail.com>
2024-12-02 13:30:52 +0100

    .

--- zadanie11_3/dummy-project/src/components/ListaLiczb.jsx
+++ zadanie11_3/dummy-project/src/components/ListaLiczb.jsx
@@ -1,8 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 
 const ListaLiczb = () => {
     const [liczby, setLiczby] = useState([1, 2, 3, 4, 5]);
     const [newNumber, setNewNumber] = useState(undefined);
+    const [suma, setSuma] = useState(undefined);
+
+    const Sum = () =>{
+        const result = liczby.reduce((total, currentValue) => total = total + currentValue,0);
+        console.log(result);
+    }
+
+    const cachedValue = useMemo(() => Sum(), liczby)
+
+    useEffect(()=>{
+        setSuma(cachedValue);
+    }, [cachedValue]);
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -20,6 +32,7 @@ const ListaLiczb = () => {
     return (
         <>
             <p>{JSON.stringify(liczby)}</p>
+            <p>Suma: {suma}</p>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="number">Dodaj liczbe: </label>
                 <input
