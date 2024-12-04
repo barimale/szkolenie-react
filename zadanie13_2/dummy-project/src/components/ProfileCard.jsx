a06a847d86c7777a78cb2769b03e5d6ffee5fe42
Mateusz <mateusz.wolnica@gmail.com>
2024-12-04 17:05:03 +0100

    .

---
+++ zadanie13_2/dummy-project/src/components/ProfileCard.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  color: black;
+  padding: 20px;
+  border-radius: 25px;
+  margin: 20px;
+  border: 1px solid black;
+  background-color: ${props => props.isHighlighted ? 'yellow' : 'transparent'}
+`;
+
+const NameHeader = styled.h1`
+    color: black;
+    padding: 20px;
+    border-radius: 5px;
+`;
+
+const DescriptionHeader = styled.h1`
+    color: black;
+    padding: 10px;
+    border-radius: 5px;
+`;
+
+
+const ProfileCard = (props) => {
+    return (
+        <>
+            <CardContainer isHighlighted={props.isHighlighted}>
+                <NameHeader>{props.name} {props.surname}</NameHeader>
+                <DescriptionHeader>{props.description}</DescriptionHeader>
+            </CardContainer>
+        </>
+    );
+}
+
+export default ProfileCard;
