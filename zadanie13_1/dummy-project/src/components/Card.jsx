87f7d92eeaef940c5f482a6d1b6e8298b560c10e
Mateusz <mateusz.wolnica@gmail.com>
2024-12-04 16:27:19 +0100

    .

--- zadanie13_1/dummy-project/src/components/Card.jsx
+++ zadanie13_1/dummy-project/src/components/Card.jsx
@@ -3,7 +3,7 @@ import styles from './Card.module.scss';
 const Card = (props) => {
     return (
         <div className={styles.card}>
-            <h1 className={[styles.header, styles.highlighted]}>{props.name} {props.surname}</h1>
+            <h1 className={[styles.header, props.isHighlighted ? styles.highlighted : undefined]}>{props.name} {props.surname}</h1>
             <h2 className={styles.content}>{props.description}</h2>
         </div>
     );
