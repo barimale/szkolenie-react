d31de319ac14906cdf10dc8594490326b16de62b
Mateusz <mateusz.wolnica@gmail.com>
2024-12-05 17:31:42 +0100

    .

---
+++ zadanie14_3/dummy-project/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { Routes, Route } from "react-router";
+
+function App() {
+  const products = [
+    { id: '1', name: 'Laptop', category: 'laptops' },
+    { id: '2', name: 'Smartphone', category: 'smartphones' },
+    { id: '3', name: 'Tablet', category: 'tablets' },
+  ];
+
+  return (
+    <>
+      <header>
+      </header>
+      <Routes>
+      </Routes>
+      <footer>
+      </footer>
+    </>
+  )
+}
+
+export default App
