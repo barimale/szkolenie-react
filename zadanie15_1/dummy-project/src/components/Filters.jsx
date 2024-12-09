3392b9bbf93552e887aac007d63872d43c48659a
Mateusz <mateusz.wolnica@gmail.com>
2024-12-09 12:04:05 +0100

    .

--- zadanie15_1/dummy-project/src/components/Filters.jsx
+++ zadanie15_1/dummy-project/src/components/Filters.jsx
@@ -1,21 +1,10 @@
-import { useState, useEffect } from 'react';
 
 const Filters = (props) => {
-    const [filter, setFilter] = useState(props.initialFilter);
-
-    const onChange = (e) =>{
-        const target = e.target;
-        setFilter(target.value);
-    }
-
-    useEffect(()=>{
-        props.OnFilterChanged(filter);
-    }, [filter]);
-
     return (
         <div style={{ border: '0px solid black', padding: '40px 20px 20px 20px' }}>
             <label>
-                Szukaj: <input name="myInput" onChange={(event)=>{onChange(event)}}/>
+                Szukaj: <input name="myInput" onChange={(event) => { props.OnFilterChanged(event.target.value) }}
+                />
             </label>
         </div>)
 }
