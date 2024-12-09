02ef8471aa797ff5b74033973714a198123d1a2e
Mateusz <mateusz.wolnica@gmail.com>
2024-12-09 17:14:51 +0100

    .

--- zadanie16_1/dummy-project/src/components/UsersList.jsx
+++ zadanie16_1/dummy-project/src/components/UsersList.jsx
@@ -4,9 +4,9 @@ const UsersList = () => {
     const {users, removeUser} = useUsers();
  
   return (
-    <div>
+    <div style= {{display: 'flex', flexWrap: 'wrap', alignItems: 'stretch', alignContent: 'space-evenly'}}>
       {users.map((user, index) => (
-        <div key={index} style={{border: '1px solid black', margin: '10px', width: '300px', borderRadius: '10px'}}>
+        <div key={index} style={{border: '1px solid black', margin: '10px', width: '270px', borderRadius: '10px'}}>
           <p style={{padding: '10px'}}>Name: <b>{user.name}</b></p>
           <button style={{margin: '10px'}} onClick={() => removeUser(user.id)}>Usuń</button>
         </div>
