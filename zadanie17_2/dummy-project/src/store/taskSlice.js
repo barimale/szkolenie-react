5e4e9a3c902663a5859ad38e7a2e8def2a2579df
Mateusz <mateusz.wolnica@gmail.com>
2024-12-10 20:26:51 +0100

    .

--- zadanie17_2/dummy-project/src/store/taskSlice.js
+++ zadanie17_2/dummy-project/src/store/taskSlice.js
@@ -3,22 +3,22 @@ import { createSlice } from "@reduxjs/toolkit";
 const taskSlice = createSlice({
   name: 'task',
   initialState: [
-    {id: Date.now() + 1, text: 'Do', state: 'ready'},
-    {id: Date.now() + 2, text: 'Did', state: 'done'},
-    {id: Date.now() + 3, text: 'Done', state: 'ready'}
+    { id: Date.now() + 1, text: 'Do', state: 'ready' },
+    { id: Date.now() + 2, text: 'Did', state: 'done' },
+    { id: Date.now() + 3, text: 'Done', state: 'ready' }
   ],
   reducers: {
     addTask: (state, action) => {
-      state.push({id: Date.now(), text: action.payload, state: 'ready'})
+      state.push({ id: Date.now(), text: action.payload, state: 'ready' })
     },
     removeTask: (state, action) => {
       return state.filter(product => product.id !== action.payload)
     },
-    toggleDone: (state, action) =>{
-      const toggledIndex = state.findIndex(p => p.id === action.payload);
+    toggleDone: (state, action) => {
+      const index = state.findIndex(p => p.id === action.payload);
       let toggled = state.find(p => p.id === action.payload);
       toggled.state = toggled.state === 'ready' ? 'done' : 'ready';
-      state[toggledIndex] = toggled;
+      state[index] = toggled;
       return state;
     }
   }
