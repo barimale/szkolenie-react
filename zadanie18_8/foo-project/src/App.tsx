a97d1e58e37288095894be6f2ebc22f7b56f01d9
Mateusz <mateusz.wolnica@gmail.com>
2024-12-12 17:11:55 +0100

    .

--- zadanie18_8/foo-project/src/App.tsx
+++ zadanie18_8/foo-project/src/App.tsx
@@ -27,12 +27,45 @@ function getTotalAmountForUser(orders: Order[], userId: number): number {
     .reduce((a, b) => a + b, 0);
 }
 
+const userA: User = {
+  id: 1,
+  name: 'ads',
+  email: 'das'
+}
+
+const users: User[] = [userA];
+
+const orders: Order[] =
+  [
+    {
+      orderId: 1,
+      userId: 1,
+      totalAmount: 100
+    },
+    {
+      orderId: 2,
+      userId: 1,
+      totalAmount: 1000
+    },
+    {
+      orderId: 3,
+      userId: 1,
+      totalAmount: 2100
+    }
+  ]
+
 function App() {
+  const result1 = getOrdersForUser(orders, 1);
+  const result2 = getUserById(users, 1);
+  const result3 = getTotalAmountForUser(orders, 1);
 
 
   return (
     <>
       <div>
+        <p>{JSON.stringify(result1)}</p>
+        <p>{JSON.stringify(result2)}</p>
+        <p>{JSON.stringify(result3)}</p>
       </div>
     </>
   )
