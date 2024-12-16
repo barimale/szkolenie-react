50866ee2632097a522632bac48405507626ee3b8
Mateusz <mateusz.wolnica@gmail.com>
2024-12-16 13:59:53 +0100

    .

--- zadanie19_1/foo-project/src/components/CustomerDetails.tsx
+++ zadanie19_1/foo-project/src/components/CustomerDetails.tsx
@@ -2,10 +2,25 @@ import { useParams, useNavigate } from 'react-router';
 import { useState, useEffect } from 'react';
 import apiClient from '../utilities/axiosClient';
 
+type Action = {
+    _id: string,
+    type: string,
+    description: string,
+    date: string,
+    customer: string
+}
+
+type CustomerDetail = {
+    _id: string,
+    name: string,
+    nip: string,
+    actions: Action[]
+}
+
 const CustomerDetails = () => {
     const navigate = useNavigate();
     const params = useParams()
-    const [item, setItem] = useState()
+    const [item, setItem] = useState<CustomerDetail>()
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState<string | null>(null)
 
@@ -25,7 +40,15 @@ const CustomerDetails = () => {
     if (error) return <p>{error}</p>
     return (
         <>
-            <p>{JSON.stringify(item)}</p>
+            <p>ID: {item?._id}</p>
+            <p>Name: {item?.name}</p>
+            <p>NIP: {item?.nip}</p>
+            <p>Actions:</p>
+            <ul>
+            {item?.actions.map((item, index)=>{
+                return <li key={index}>{item._id}</li>
+            })}
+            </ul>
             <button onClick={() => { navigate('/') }}>Wróć</button>
         </>);
 }
