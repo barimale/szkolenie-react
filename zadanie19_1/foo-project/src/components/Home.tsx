235084c772fbb85955269f095cbeb7c64f73b55e
Mateusz <mateusz.wolnica@gmail.com>
2024-12-16 13:46:07 +0100

    .

--- zadanie19_1/foo-project/src/components/Home.tsx
+++ zadanie19_1/foo-project/src/components/Home.tsx
@@ -1,15 +1,21 @@
 import apiClient from '../utilities/axiosClient';
 import { useEffect, useState } from 'react';
 
+type Customer = {
+    _id: string,
+    name: string,
+    npm: string,
+    actions: string[]
+}
 const Home = () => {
-    const [items, setItems] = useState([]);
+    const [items, setItems] = useState<Customer[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
-        apiClient.get('/customers')
+        apiClient.get(`/customers?page=${1}&limit=${20}`)
             .then(response => {
-                setItems(response.data)
+                setItems(response.data.data)
                 setLoading(false)
             })
             .catch(error => {
@@ -24,7 +30,9 @@ const Home = () => {
     return (
         <>
             <h1>Home</h1>
-            <h2>{JSON.stringify(items)}</h2>
+            <h2>{items.map((item, index)=>{
+                return <p style={{cursor: 'pointer'}} >{item.name}</p>
+            })}</h2>
         </>)
 }
 
