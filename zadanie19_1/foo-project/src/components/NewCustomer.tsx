41ea27bde432529168fb4708683f701516e061f7
Mateusz <mateusz.wolnica@gmail.com>
2024-12-24 21:21:33 +0100

    .

--- zadanie19_1/foo-project/src/components/NewCustomer.tsx
+++ zadanie19_1/foo-project/src/components/NewCustomer.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, FormEvent, useState } from "react";
-import { Link, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import { Customer } from "./Home";
 import axiosClient from "../utilities/axiosClient";
 
