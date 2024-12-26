77225c57e505205c68d9777b87ecd485e66658f9
Mateusz <mateusz.wolnica@gmail.com>
2024-12-26 13:22:50 +0100

    .

--- zadanie19_1/foo-project/src/components/CustomerDetails.tsx
+++ zadanie19_1/foo-project/src/components/CustomerDetails.tsx
@@ -79,6 +79,7 @@ const CustomerDetails = () => {
                             <h4>ID: {subitem._id}</h4>
                             <p>Opis: {subitem.description}</p>
                             <p>Data: {subitem.date}</p>
+                            <p>Typ: {subitem.type}</p>
                             <button onClick={() => { editAction(subitem._id, item._id) }}>Edytuj akcję</button>
                             <button onClick={() => { removeAction(subitem._id) }}>Usuń akcję</button>
                         </li>);
